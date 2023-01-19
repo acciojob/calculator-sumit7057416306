ec34479278d858f258081da57353bb4b2f0d21d2
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-01-19 11:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let input = document.getElementById("input");
+let buttons = document.getElementsByTagName("button");
+
+let firstVal = "";
+let secondVal = "";
+let result = "";
+let operator = "";
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    let btnVal
+    let btnVal = this.innerHTML;
+
+    if (btnVal === "AC") {
+      first
